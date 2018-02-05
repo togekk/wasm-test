f16f82deac3e8985bf4ff795bd0e42f9d98342cd
togekk <togekk@gmail.com>
2018-02-06 00:11:12 +0800

    Update index.js

--- string_coversion/index.js
+++ string_coversion/index.js
@@ -1,9 +1,12 @@
-var MyCode = require('./string.js');
+var MyCode = require('./string.js');                                      // import modularized 'MyCode' function
+
 MyCode().then(Module => {
+    
     // Get string from C++
     const offset = Module._getOffset();                                   // get memory address
     const len = Module._getLen();                                         // get string length
-    console.log(Module.Pointer_stringify(offset, len));                   // use 'Pointer_stringify' function to convert to javascript string
+    const str = Module.Pointer_stringify(offset, len);                    // use 'Pointer_stringify' function to convert to javascript string
+    console.log(str)
 
     // Send string to C++
     const string = 'String from JavaScript';
@@ -11,4 +14,5 @@ MyCode().then(Module => {
     const stringOnWasmHeap = Module.getMemory(lengthBytes);               // get memory address in wasm heap
     Module.stringToUTF8(string, stringOnWasmHeap, lengthBytes + 1);       // use 'stringToUTF8' funciton to write string in wasm heap
     Module._getString(stringOnWasmHeap);                                  // call c function with memory address
+    
 })
