82dfeb2dcc0cd99f3a8a83179a9eb965f50fed58
togekk <togekk@gmail.com>
2018-02-05 23:56:31 +0800

    Update string.cpp

--- string_coversion/string.cpp
+++ string_coversion/string.cpp
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdio.h>   // for 'printf' function
+#include <string.h>  // for 'strlen' function
+#include <stdlib.h>  // for 'free' function
 
 extern "C" {
 
@@ -22,18 +22,5 @@ void getString(char *str)
     printf("%s\n", str);
     free(str);
 }
-
-// int *append(int *str, int len)
-// {
-//     // int a;
-//     // for (int i = len + 1; i < len + 5; i++)
-//     // {
-//     //     str[i] = 72;
-//     // }
-//     return &str[1];
-//     // strcat(str, " fuck");
-//     // char *str = buffer.data;
-//     // size_t len = strlen(str);
-//     // return str;
-// }
+    
 }
