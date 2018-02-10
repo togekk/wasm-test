3aa1450f0dfeb7b9c6d7b2b4fd46e711de3e6376
togekk <togekk@gmail.com>
2018-02-10 20:10:41 +0800

    Add array_index_of_object_array

---
+++ array_index_of_object_array/index.js
@@ -0,0 +1,57 @@
+var MyCode = require('./string.js');                                      // import modularized 'MyCode' function
+
+let mod;
+
+MyCode().then(Module => {
+
+    mod = Module;
+
+    const string_object = [
+        {
+            name: "Simon",
+            age: "30",
+            nationality: "Taiwan",
+            date_of_birth: "01/01/1900"
+        },
+        {
+            name: "John",
+            age: "27",
+            nationality: "Ireland",
+            date_of_birth: "03/03/1900"
+        },
+        {
+            name: "David",
+            age: "23",
+            nationality: "Germany",
+            date_of_birth: "05/05/1900"
+        },
+        {
+            name: "Peter",
+            age: "25",
+            nationality: "Australia",
+            date_of_birth: "07/07/1900"
+        }
+    ];
+
+    const value = "Simon";
+
+    console.log(arrayIndexOf(string_object, value));
+})
+
+function send_string(string) {
+    const lengthBytes = mod.lengthBytesUTF8(string) + 1;               // get string length in bytes
+    const stringOnWasmHeap = mod.getMemory(lengthBytes);               // get memory address in wasm heap
+    mod.stringToUTF8(string, stringOnWasmHeap, lengthBytes + 1);       // use 'stringToUTF8' funciton to write string in wasm heap
+    return stringOnWasmHeap;
+}
+
+
+function arrayIndexOf(obj_arr, value) {
+    const obj = JSON.stringify(obj_arr);
+    const obj_heap = send_string(obj);
+    const str_heap = send_string(value);
+    const id = mod._arrayIndexOf(obj_heap, str_heap, obj_arr.length, Object.keys(obj_arr).length);             // call c function with memory address
+    return id;
+    // const len = mod._getLen(offset);                                         // get string length
+    // return mod.Pointer_stringify(offset, len);                    // use 'Pointer_stringify' function to convert to javascript string
+}
---
+++ array_index_of_object_array/string.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>  // for 'printf' function
+#include <string.h> // for 'strtok' & 'strcmp'
+#include <stdlib.h> // for 'free' function
+
+using namespace std;
+
+extern "C" {
+
+// get object array from javascript
+
+int arrayIndexOf(char* arr, char* value_selected, int arr_length, int key_length)
+{
+    char* a;
+    int id = 0;
+    int id_found = -1;
+    
+    strtok(arr, "\"");
+
+    do {
+
+        a = strtok(NULL, "{,\"}");
+        if (strcmp(a, value_selected) == 0) {
+            id_found = id;
+        }
+        id++;
+
+    } while (strcmp(a, value_selected) != 0 && id < arr_length * key_length * 3);
+
+    if (id_found >= 0) {
+        return id_found / key_length / 3;
+    } else {
+        return -1;
+    }
+    
+    free(value_selected);
+    free(arr);
+}
+}
