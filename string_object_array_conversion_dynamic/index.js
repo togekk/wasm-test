be8159797496907d393f2c191895aa1314f9bfed
togekk <togekk@gmail.com>
2018-02-14 12:39:29 +0800

    Using JS loop

--- string_object_array_conversion_dynamic/index.js
+++ string_object_array_conversion_dynamic/index.js
@@ -41,11 +41,45 @@ function send_string(string) {
 }
 
 
-function get_value_of_object(obj_arr, id, key) {
-    const obj = JSON.stringify(obj_arr);
-    const obj_heap = send_string(obj);
-    const str_heap = send_string(key);
-    const offset = mod._getObjectFromJS(obj_heap, Object.keys(obj_arr[0]).length, obj_arr.length, id, str_heap);             // call c function with memory address
+function get_value_from_object(offset) {
     const len = mod._getLen(offset);                                         // get string length
     return mod.Pointer_stringify(offset, len);                    // use 'Pointer_stringify' function to convert to javascript string
+}
+
+function arrayToHeap(typedArray) {
+    var numBytes = typedArray.length * typedArray.BYTES_PER_ELEMENT;
+    var ptr = mod.getMemory(numBytes);
+    var heapBytes = new Uint8Array(mod.HEAPU8.buffer, ptr, numBytes);
+    heapBytes.set(new Uint8Array(typedArray.buffer));
+    return heapBytes;
+}
+
+function get_value_of_object(obj_arr, id, key) {
+    let count = 0;
+    const key_selected = send_string(key);
+    const key_heap = [];
+    const obj_heap = [];
+    obj_arr.forEach((obj_each, index) => {
+        // if (index2 === 0) {
+        Object.keys(obj_each).forEach((key_each) => {
+            key_heap[index] = [];
+            obj_heap[index] = [];
+            key_heap[index][key_each] = send_string(key_each);
+            obj_heap[index][key_each] = send_string(obj_each[key_each]);
+            mod._getObjectFromJS(key_heap[index][key_each], obj_heap[index][key_each], obj_arr.length, Object.keys(obj_arr[0]).length);
+        });
+        // }
+    });
+
+    const offset = mod._getValueFromObject(id, send_string(key));
+    const result = get_value_from_object(offset);
+
+    obj_arr.forEach((obj_each, index) => {
+        Object.keys(obj_each).forEach((key_each) => {
+            mod._free(key_heap[index][key_each]);
+            mod._free(obj_heap[index][key_each]);
+        });
+    });
+
+    return result;
 }
--- string_object_array_conversion_dynamic/string.cpp
+++ string_object_array_conversion_dynamic/string.cpp
@@ -1,9 +1,9 @@
-#include <stdio.h>  // for 'printf' function
+#include <stdio.h> // for 'printf' function
 // #include <string.h> // for 'strlen' function
-#include <string> // for 'string'
+#include <string>   // for 'string'
 #include <stdlib.h> // for 'free' function
-#include <map> // for 'map' type
-#include <vector> // for 'vector' type
+#include <map>      // for 'map' type
+#include <vector>   // for 'vector' type
 
 using namespace std;
 
@@ -11,60 +11,34 @@ extern "C" {
 
 // get length of the string
 
-size_t getLen(char* s)
+size_t getLen(char *s)
 {
     size_t len = strlen(s);
     return len;
 }
 
+// vector<string> key_arr;
+// typedef map<string, const char *> obj_type;
+// obj_type obj;
+// vector<obj_type> obj_arr;
+// int count_id = 0;
+
 // get object array from javascript
+vector<string> key;
+int value_id = 0;
+typedef map<pair<int, string>, const char *> obj_type;
+obj_type obj;
 
-char* getObjectFromJS(char* arr, int key_length, int array_length, int array_index, char* key_selected)
+void getObjectFromJS(char *key_2, char *value, int array_length, int object_length)
 {
+    key.push_back(key_2);
+    obj[make_pair(value_id / 4, key[value_id])] = value;
+    value_id++;
+}
 
-    vector<string> key;
-    char **value = new char *[key_length];
-    int value_id;
-    char* a;
-    char* b;
-    typedef map<string, const char *> obj_type;
-    obj_type obj;
-    vector<obj_type> obj_arr;
-
-    for (int i = 0; i < array_length; i++) {
-        value_id = 0;
-
-        for (int j = 0; j < key_length *  3 + 1; j++)
-        {
-            if (j == 0) {
-                if (i == 0)
-                {
-                    strtok(arr, "\"");
-                }
-            } else {
-                if (j % 3 == 1) {
-                    b = strtok(NULL, "{,\"}");
-                    key.push_back(b);
-                } else {
-                    if (j % 3 == 0) {
-                        a = strtok(NULL, "{,\"}");
-                        value[value_id] = a;
-                        obj[key[value_id]] = value[value_id];
-                        value_id++;
-                    } else {
-                        strtok(NULL, "{,\"}");
-                    }
-                }
-            }
-        };
-
-        obj_arr.push_back(obj);
-    };
-
-    char *result = (char*)obj_arr[array_index][key_selected];
-    return result;
-    
+char *getValueFromObject(int array_index, char *key_selected)
+{
+    return (char *)obj[make_pair(array_index, key_selected)];
     free(key_selected);
-    free(arr);
 }
 }
